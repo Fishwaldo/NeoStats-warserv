173fa3cc19f89a843743350ec8c2c51fc1b36702
Fish <>
2008-03-10 07:35:31 +0000

    WarServ for Win32

---
+++ win32/buildwin32snap.bat
@@ -0,0 +1,4 @@
+@echo off
+IF EXIST win32\WarServ-Setup*.exe del win32\WarServ-Setup*.exe
+"C:\Program Files\Pantaray\QSetup\Composer.exe" win32\WarServ.qsp /Compile
+move win32\WarServ-Setup.exe win32\WarServ-Setup-3-0-a4-%1.exe
